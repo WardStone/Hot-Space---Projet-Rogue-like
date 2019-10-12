f9b9f6c57e126ba4a94e01910ae2bf9010155414
Job Corentin <job.corentin@outlook.fr>
2019-10-12 14:51:28 +0200

    Character Animation Added, Tuto Boss pattern work in progress (BASE FILE)

---
+++ Roguelike_CharacterControll/Assets/COJ_Script/CharacterArmsBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterArmsBehaviour : MonoBehaviour
+{
+    private Animator armsAnimator;
+    void Start()
+    {
+        armsAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        armsAnimator.SetFloat("HorizontalJoystick", Input.GetAxis("HorizontalSecondJoystick"));
+        armsAnimator.SetFloat("VerticalJoystick", Input.GetAxis("VerticalSecondJoystick"));
+    }
+}
---
+++ Roguelike_CharacterControll/Assets/COJ_Script/GunBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+    public Transform aimingPoint;
+    public Rigidbody2D rb;
+    public GameObject weaponPointPosition;
+
+    Vector2 aimingPointPos;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        aimingPointPos = aimingPoint.transform.position;
+        Vector2 lookDir = aimingPointPos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        rb.position = weaponPointPosition.transform.position;
+
+    }
+}

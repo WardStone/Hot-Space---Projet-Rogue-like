2726cf79ec3c158854317bd8b5cec9ce75eb5bfd
PARET Paul <p.paret@rubika-edu.com>
2019-11-04 10:11:38 +0100

    enviro meca

--- Roguelike_CharacterControll/Assets/PAP_Script/FallingRocks.cs
+++ Roguelike_CharacterControll/Assets/PAP_Script/FallingRocks.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class FallingRocks : MonoBehaviour
 {
-    public int time = 1;
+    public GameObject myPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject rocks = Instantiate(myPrefab, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
        
-           ;
     }
 }

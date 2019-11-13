6d95a616daf311811724eac911ded3979863e699
PARET Paul <p.paret@rubika-edu.com>
2019-11-13 18:05:11 +0100

    Lost my last commit so player can't destroy the rock anymore ;(

--- Roguelike_CharacterControll/Assets/PAP_Script/BrkRockBehavior.cs
+++ Roguelike_CharacterControll/Assets/PAP_Script/BrkRockBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BrkRockExpl : MonoBehaviour
+public class BrkRockBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

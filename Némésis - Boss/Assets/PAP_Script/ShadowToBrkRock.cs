c7ea79e553da7b8015480bda5137769bef3d47ad
PARET Paul <p.paret@rubika-edu.com>
2019-11-13 18:25:20 +0100

    Added Enviro_Paul_16_10_2019 Onto the new prototype

---
+++ Némésis - Boss/Assets/PAP_Script/ShadowToBrkRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowToBrkRock : MonoBehaviour
+{
+    public GameObject brkRock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject rock = Instantiate(brkRock, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Némésis - Boss/Assets/PAP_Script/ShadowToStyRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowToStyRock : MonoBehaviour
+{
+    public GameObject stayingRock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject rock = Instantiate(stayingRock, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        Destroy(gameObject);
+        
+    }
+}

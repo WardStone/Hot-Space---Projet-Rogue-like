d7592ae4be7be4732c99ad8703869216fe7c382b
Simon <s.picardat@rubika-edu.com>
2020-01-05 15:40:47 +0100

    Ajout gen procédurale et tp

---
+++ Némésis - Boss/Assets/SPI_Scripts/RoomDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDetection : MonoBehaviour
+{
+
+    public GameObject rootRoom;
+    public GameObject tpManager;
+    public GameObject currentRoom;
+
+    void Start()
+    {
+        rootRoom = GameObject.FindGameObjectWithTag("RootRoom");
+        tpManager = rootRoom.transform.GetChild(0).gameObject; // enfant de la salle racine qui sert juste de detecteur/ lock
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentRoom.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
---
+++ Némésis - Boss/Assets/SPI_Scripts/teleport.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public GameObject tp;
+    public GameObject player;
+    public bool canTp;
+    public int orientation;
+
+    public GameObject tpCoridor;
+
+    public float distanceV;
+    public float distanceH;
+
+    void Start()
+    {
+        distanceH = 25f;
+        distanceV = 25f;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+
+        if (canTp & Input.GetButtonDown("Interact"))
+        {
+            Debug.Log("tp toi !!!!");
+            switch (orientation)
+            {
+                case 0: // tp north
+                    //player.transform.localPosition = new Vector2 (distanceH,0);
+                    player.transform.localPosition = tpCoridor.transform.GetChild(1).gameObject.transform.GetChild(2).gameObject.transform.position;
+                    break;
+
+                case 1: //tp east
+                    //player.transform.localPosition = new Vector2(0, distanceV);
+                    player.transform.localPosition = tpCoridor.transform.GetChild(0).gameObject.transform.GetChild(2).gameObject.transform.position;
+                    break;
+
+                case 2: //tp south
+                    //player.transform.localPosition = new Vector2(-distanceH, 0);
+                    player.transform.localPosition = tpCoridor.transform.GetChild(0).gameObject.transform.GetChild(2).gameObject.transform.position;
+                    break;
+
+                case 3:// tp west
+                    //player.transform.localPosition = new Vector2(0, -distanceV);
+                    player.transform.localPosition = tpCoridor.transform.GetChild(1).gameObject.transform.GetChild(2).gameObject.transform.position;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            canTp = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            canTp = false;
+    }
+}

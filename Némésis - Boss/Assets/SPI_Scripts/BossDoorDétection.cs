6cc9a6d5bf22d47464ce8531d06eb3fe3ca3b077
Simon <s.picardat@rubika-edu.com>
2020-01-06 16:21:34 +0100

    Add script door/key

--- Némésis - Boss/Assets/SPI_Scripts/BossDoorDétection.cs
+++ Némésis - Boss/Assets/SPI_Scripts/BossDoorDétection.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossDoorDétection : MonoBehaviour
 {
     public bool canOpenBoss;
+    public int keyLeft;
 
     void Start()
     {
@@ -13,9 +15,17 @@ public class BossDoorDétection : MonoBehaviour
 
     void Update()
     {
+
+        keyLeft = GameObject.FindGameObjectsWithTag("Key").Length;
+
+        if (keyLeft == 0)
+        {
+            canOpenBoss = true;
+        }
+
         if (canOpenBoss & Input.GetButtonDown("Interact"))
         {
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 

f94ff251e32251745c79eb44641d3ecbb942020e
Job Corentin <job.corentin@outlook.fr>
2020-01-08 00:06:40 +0100

    Boss health bar + Wait before bossSpawn in the boss Scene

---
+++ Némésis - Boss/Assets/COJ_Script/BossSpawnerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawnerBehaviour : MonoBehaviour
+{
+
+    public GameObject boss;
+    SimpleCameraShakeInCinemachine camShake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camShake = GameObject.FindGameObjectWithTag("CameraShakeManager").GetComponent<SimpleCameraShakeInCinemachine>();
+        StartCoroutine(SpawnBoss());
+    }
+
+   IEnumerator SpawnBoss()
+    {
+        camShake.Shake();
+        yield return new WaitForSeconds(3.5f);
+        camShake.Shake();
+        yield return new WaitForSeconds(1f);
+        camShake.Shake();
+        yield return new WaitForSeconds(0.5f);
+        camShake.Shake();
+        Instantiate(boss, gameObject.transform.position, Quaternion.identity);
+    }
+}

e1c6fbabffe6397cb9f92887af9a9e05c27f414a
Simon <s.picardat@rubika-edu.com>
2020-01-07 10:05:00 +0100

    Anim Main Menu

---
+++ Némésis - Boss/Assets/SPI_Scripts/ArriveInScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArriveInScene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject loadingScreenAnim;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        loadingScreenAnim = GameObject.FindGameObjectWithTag("loadScreen");
+        StartCoroutine(loadingScreen());
+    }
+
+    IEnumerator loadingScreen()
+    {
+        player.SetActive(false);
+        //loadingScreenAnim.SetActive(true);//Set loading screen active true
+        yield return new WaitForSeconds(4f);
+        player.SetActive(true);
+        player.transform.position = gameObject.transform.localPosition;
+        loadingScreenAnim.SetActive(false);//Set loading screen active true
+    }
+}

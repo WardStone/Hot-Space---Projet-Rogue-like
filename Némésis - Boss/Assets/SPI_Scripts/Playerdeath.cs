f7cd4449ea4a05fd916d7bbf09dc13be4babe225
Simon <s.picardat@rubika-edu.com>
2020-01-08 13:31:06 +0100

    Fix Tp et add restart

---
+++ Némésis - Boss/Assets/SPI_Scripts/Playerdeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerdeath : MonoBehaviour
+{
+    public bool wannaRestartBoi;
+    public GameObject player;
+    void Start()
+    {
+
+        
+    }
+    void Update()
+    {
+        if (wannaRestartBoi)
+        {
+            if (Input.GetButtonDown("Interact") || Input.GetButtonDown("Pause"))
+            {
+
+            }
+        }
+    }
+
+    public IEnumerator playerDies()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //player.transform.GetChild(tamère).gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        wannaRestartBoi = true;
+        yield break;
+    }
+}
---
+++ Némésis - Boss/Assets/SPI_Scripts/StartLoadingScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLoadingScreen : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+}
